6e44b824bb30fa1621b0cac5d0bf20de95300600
junhui <junhuilim@kakao.com>
2026-01-30 03:06:28 +0900

    feat: testcontroller 구현

---
+++ src/main/java/com/scorenow/scorenow_api/external/betsapi/controller/BetsApiTestController.java
@@ -0,0 +1,36 @@
+package com.scorenow.scorenow_api.external.betsapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scorenow.scorenow_api.external.betsapi.BetsApiClient;
+import com.scorenow.scorenow_api.external.betsapi.dto.BetsEventResponse;
+import com.scorenow.scorenow_api.global.dto.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/test/betsapi")
+@RequiredArgsConstructor
+public class BetsApiTestController {
+
+	private final BetsApiClient betsApiClient;
+
+	@GetMapping("/upcoming")
+	public ApiResponse<BetsEventResponse> testUpcoming(
+		@RequestParam(defaultValue = "1") String sportId,
+		@RequestParam(required = false) String day,
+		@RequestParam(defaultValue = "1") int page) {
+
+		return ApiResponse.success(betsApiClient.getUpcomingEvents(sportId, day, page));
+	}
+
+	@GetMapping("/inplay")
+	public ApiResponse<BetsEventResponse> testInplay(
+		@RequestParam(defaultValue = "1") String sportId) {
+
+		return ApiResponse.success(betsApiClient.getInplayEvents(sportId));
+	}
+}
