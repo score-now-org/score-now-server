c9b6386164b379a1e05da864f6ff44bb0d1b2b78
junhui <junhuilim@kakao.com>
2026-01-30 03:06:13 +0900

    feat: ApiClient 구현 및 properties 설정

---
+++ src/main/java/com/scorenow/scorenow_api/external/betsapi/BetsApiProperties.java
@@ -0,0 +1,17 @@
+package com.scorenow.scorenow_api.external.betsapi;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@ConfigurationProperties(prefix = "betsapi")
+@Getter
+@Setter
+public class BetsApiProperties {
+	private String baseUrl;
+	private String token;
+	private int timeout;
+}
