3ff3e931d298b09c2a2653527ce7038145ee4b20
junhui <junhuilim@kakao.com>
2026-02-22 21:07:35 +0900

    feat: 리그 등록/수정/삭제 API 구현

---
+++ src/main/java/com/scorenow/scorenow_api/domain/league/dto/request/LeagueCreateRequest.java
@@ -0,0 +1,19 @@
+package com.scorenow.scorenow_api.domain.league.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LeagueCreateRequest {
+
+	@NotBlank
+	private String sportId;
+
+	@NotBlank
+	private String leagueId; // 외부 ID(BetsAPI 기준)
+
+	@NotBlank
+	private String kName;
+
+	private String sName;
+}
