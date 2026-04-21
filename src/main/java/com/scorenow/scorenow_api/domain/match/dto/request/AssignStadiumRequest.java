c4cfc819a91e55607c4a018f7bd74f4c7a6d72d0
zoolake <junhomoon224@gmail.com>
2026-04-21 22:28:17 +0900

    refactor : stadium 관련 dto 스웨거 적용

--- src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/AssignStadiumRequest.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/AssignStadiumRequest.java
@@ -1,5 +1,6 @@
 package com.scorenow.scorenow_api.domain.match.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class AssignStadiumRequest {
+    @Schema(description = "임시 경기장명", example = "수원 월드컵 경기장")
     @NotNull(message = "경기장 ID는 필수입니다.")
     private Long stadiumId;
 }
--- src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/AssignTemporaryStadiumRequest.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/AssignTemporaryStadiumRequest.java
@@ -1,5 +1,6 @@
 package com.scorenow.scorenow_api.domain.match.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class AssignTemporaryStadiumRequest {
-    @NotBlank(message = "임시 경기장 이름은 필수입니다.")
+    @Schema(description = "임시 경기장명", example = "수원 월드컵 경기장")
+    @NotBlank(message = "임시 경기장명은 필수입니다.")
     private String stadiumName;
-    
+
+    @Schema(description = "도시명", example = "수원")
     private String city;
 }
