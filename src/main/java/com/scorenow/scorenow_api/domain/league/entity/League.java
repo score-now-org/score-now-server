d81c4254f76f31aa57958893b4c92aa5c7d50292
hailyn-k <jhh48654@gmail.com>
2026-05-10 13:37:33 +0900

    fix: update unique constraints for external mappings

--- src/main/java/com/scorenow/scorenow_api/domain/league/entity/League.java
+++ src/main/java/com/scorenow/scorenow_api/domain/league/entity/League.java
@@ -2,26 +2,40 @@ package com.scorenow.scorenow_api.domain.league.entity;
 
 import com.scorenow.scorenow_api.global.entity.BaseEntity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "leagues")
+@Table(name = "leagues",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "uk_leagues",
+			columnNames = {"sport_id", "e_name"})})
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class League extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long sportId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Long sportId;
 
-    private String kName;
+	private String kName;
 
-    private String eName;
-    private String sName; // 숏네임
+	private String eName;
+	private String sName; // 숏네임
 
-    private String cc;
+	private String cc;
 }
