ed8bbd8e82b0426b1efbdcc76f0cf4da7f2e9172
zoolake <junhomoon224@gmail.com>
2026-05-15 17:25:25 +0900

    chore : 주석 다듬기

--- src/main/java/com/scorenow/scorenow_api/domain/match/service/InplayMatchSyncService.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/service/InplayMatchSyncService.java
@@ -32,7 +32,7 @@ public class InplayMatchSyncService {
     public void syncInplayMatches() {
         ZonedDateTime now = LocalDateTime.now().atZone(ZoneId.of(SEOUL_TIME_ZONE));
 
-        // 1. 현재 INPLAY 일 것으로 예상되는 경기들을 조회
+        // 1. 동기화 대상 조회 (현재 시간 기준 INPLAY 일 것으로 예상되는 경기들)
         Set<Object> matchCandidates = redisTemplate.opsForZSet()
                 .rangeByScore(INPLAY_CANDIDATES_KEY, 0, (double) now.plusSeconds(1).toInstant().toEpochMilli());
 
@@ -45,24 +45,22 @@ public class InplayMatchSyncService {
                 .map(obj -> (MatchCandidate) obj)
                 .toList();
 
-        // 2. 외부 sportId 값만 추출 (종목이 여러 개면 각각 INPLAY API 를 호출해야 하기 때문)
+        // 2. 외부 API 중복 호출 방지를 위해 Sport ID만 추출 (종목별 INPLAY API 호출 필요)
         List<String> apiSportIds = candidates.stream()
                 .map(MatchCandidate::getApiSportId)
                 .distinct()
                 .toList();
 
-        // 2. 종목 별 INPLAY API 요청 후, 응답 받은 Match ID 만 취합
+        // 3. 종목별 INPLAY API 호출 및 실제 진행 중인 경기 ID 취합
         Set<String> inplayMatchIds = apiSportIds.stream()
                 .map(sportId -> betsApiClient.getInplayEvents(sportId, null))
                 .flatMap(response -> response.getResults().stream())
                 .map(BetsEventResponse.Event::getId)
                 .collect(Collectors.toSet());
 
-        /**
-         * 3. INPLAY API 응답과 비교하며, 실제로 경기가 진행된 경기와 경기가 유예기간이 초과한 경기를 분류
-         *    참고) 유예기간이 초과한 경기:
-         *          현재 시간 기준 시작되어야 하는 경기이나 MAX_GRACE_PERIOD 만큼 대기 후에도 API 응답 상으로 여전히 시작하지 못한 경기
-         */
+
+        // 4. INPLAY API 응답과 대조하여 대상 분류 (진행 중, 유예기간 초과)
+        // 참고) 유예기간 초과한 경기 : 현재 시간 기준 시작돼야 하는 경기이나, 최대 유예 기간만큼 대기 후에도 API 응답 기준 여전히 시작하지 못한 경기
         List<MatchCandidate> inplayMatches = new ArrayList<>();
         List<MatchCandidate> toBeFixedMatches = new ArrayList<>();
 
@@ -79,7 +77,7 @@ public class InplayMatchSyncService {
             }
         }
 
-        // 4. 분류 결과를 기반으로 Redis 와 DB 에 반영
+        // 5. 분류 결과를 기반으로 DB 반영 및 Redis 캐시 정리
         if (!inplayMatches.isEmpty() || !toBeFixedMatches.isEmpty()) {
             inplayMatchStatusUpdater.updateMatchStatuses(inplayMatches, toBeFixedMatches);
             removeFromZSet(inplayMatches);
