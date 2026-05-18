e1888642dc3de58b8e04633503b04581e80801a3
zoolake <junhomoon224@gmail.com>
2026-05-18 13:12:05 +0900

    refactor : 기본 생성자가 없어서 발생한 역직렬화 에러 대응

--- src/main/java/com/scorenow/scorenow_api/domain/match/dto/MatchCandidate.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/dto/MatchCandidate.java
@@ -1,24 +1,23 @@
 package com.scorenow.scorenow_api.domain.match.dto;
 
 import com.scorenow.scorenow_api.external.common.ApiProvider;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.EqualsAndHashCode.Include;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class MatchCandidate {
     @Include
-    private final Long matchId;
-    private final Long sportId;
+    private Long matchId;
+    private Long sportId;
 
-    private final ApiProvider provider;
-    private final String apiMatchId;
-    private final String apiSportId;
+    private ApiProvider provider;
+    private String apiMatchId;
+    private String apiSportId;
 
-    private final LocalDateTime startAt;
+    private LocalDateTime startAt;
 }
