32171e06151400911989de46749548c9b67a45cf
zoolake <junhomoon224@gmail.com>
2026-05-29 00:28:56 +0900

    chore : 변수명 변경

--- src/main/java/com/scorenow/scorenow_api/domain/match/service/MatchDetailService.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/service/MatchDetailService.java
@@ -31,23 +31,23 @@ public class MatchDetailService {
     private final MatchDetailRepository matchDetailRepository;
 
     @Transactional
-    public void updateInplayMatchDetail(ApiProvider provider, ViewResult matchDetail) {
-        String apiMatchId = matchDetail.getId();
+    public void updateInplayMatchDetail(ApiProvider provider, ViewResult viewResult) {
+        String apiMatchId = viewResult.getId();
 
         Match match = matchRepository.findByExternalInfo(provider, apiMatchId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.MATCH_NOT_FOUND));
 
-        MatchStatus matchStatus = MatchStatus.fromCode(matchDetail.getTimeStatus());
+        MatchStatus matchStatus = MatchStatus.fromCode(viewResult.getTimeStatus());
         if (matchStatus != NOT_STARTED) {
             match.updateStatus(matchStatus);
         }
-        match.updateHomeScore(matchDetail.getHomeScore());
-        match.updateAwayScore(matchDetail.getAwayScore());
+        match.updateHomeScore(viewResult.getHomeScore());
+        match.updateAwayScore(viewResult.getAwayScore());
 
         // 경기장 정보 생성 및 변경 (로컬 캐시 활용 + 더티체킹)
-        StadiumData stadiumData = matchDetail.getExtra().getStadiumData();
+        StadiumData stadiumData = viewResult.getExtra().getStadiumData();
         if (stadiumData != null) {
-            Stadium stadium = stadiumCacheService.getOrCreateStadium(provider, matchDetail.getSportId(), stadiumData.getId(), stadiumData.getName(), stadiumData.getCity());
+            Stadium stadium = stadiumCacheService.getOrCreateStadium(provider, viewResult.getSportId(), stadiumData.getId(), stadiumData.getName(), stadiumData.getCity());
 
             // 경기 정보에 경기장 정보가 할당되어 있지 않은 경우에만 할당
             if (match.isStadiumEmpty()) {
@@ -56,8 +56,8 @@ public class MatchDetailService {
         }
 
         // 경기 정보 반영
-        MatchDetailDocument detail = matchDetail.toDocument(match.getId());
-        matchDetailRepository.upsertMatchDetail(detail);
+        MatchDetailDocument matchDetailDocument = viewResult.toDocument(match.getId(), match.getStartAt());
+        matchDetailRepository.upsertMatchDetail(matchDetailDocument);
 
         log.info("✅ 경기 상세 데이터(MySQL & MongoDB) 동기화 완료 matchId:{}, apiMatchId:{}", match.getId(), apiMatchId);
     }
