49ec76a3dc2d04febbde9eb7928cde20da9d0835
zoolake <junhomoon224@gmail.com>
2026-05-29 00:33:45 +0900

    chore : 미사용 import 구문 삭제

--- src/main/java/com/scorenow/scorenow_api/domain/match/document/MatchDetailDocument.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/document/MatchDetailDocument.java
@@ -9,7 +9,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
