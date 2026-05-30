028135e9df7b43ceff699c7b82208633da0a1394
zoolake <junhomoon224@gmail.com>
2026-05-30 23:09:00 +0900

    chore : 미사용 import 구문 삭제

--- src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/MatchCreateRequest.java
+++ src/main/java/com/scorenow/scorenow_api/domain/match/dto/request/MatchCreateRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
