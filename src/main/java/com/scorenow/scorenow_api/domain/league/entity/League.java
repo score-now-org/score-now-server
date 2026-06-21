f9a51ceca41ebe0703bafd119f1fd240b3710ecc
zoolake <junhomoon224@gmail.com>
2026-06-22 02:09:21 +0900

    chore : todo 삭제

--- src/main/java/com/scorenow/scorenow_api/domain/league/entity/League.java
+++ src/main/java/com/scorenow/scorenow_api/domain/league/entity/League.java
@@ -11,12 +11,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
-/**
- * TODO : unique 제약 조건 변경 해야 할 듯?
- *             이제는 season 정보를 같이 담기 때문에 EPL 24-25 / EPL 25-26 은 다른 리그로 봐야 함.
- */
-
 @Entity
 @Table(name = "leagues",
         uniqueConstraints = {
