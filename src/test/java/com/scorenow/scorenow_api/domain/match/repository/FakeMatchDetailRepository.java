ee1733e37b521755e33ca9919969dc796408d16e
zoolake <junhomoon224@gmail.com>
2026-08-05 13:43:45 +0900

    refactor : 테스트 코드 수정

--- src/test/java/com/scorenow/scorenow_api/domain/match/repository/FakeMatchDetailRepository.java
+++ src/test/java/com/scorenow/scorenow_api/domain/match/repository/FakeMatchDetailRepository.java
@@ -1,7 +1,6 @@
 package com.scorenow.scorenow_api.domain.match.repository;
 
 import com.scorenow.scorenow_api.domain.match.document.MatchDetailDocument;
-import com.scorenow.scorenow_api.domain.match.document.MatchDetailDocument.AdditionalTime;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.*;
@@ -62,57 +61,10 @@ public class FakeMatchDetailRepository implements MatchDetailRepository {
             return;
         }
 
+        ReflectionTestUtils.setField(saved, "type", matchDetailDocument.getType());
         ReflectionTestUtils.setField(saved, "homeScore", matchDetailDocument.getHomeScore());
-        if (matchDetailDocument.getHomeShootOutScore() != null) {
-            ReflectionTestUtils.setField(saved, "homeShootOutScore", matchDetailDocument.getHomeShootOutScore());
-        }
-        ReflectionTestUtils.setField(saved, "homeStats", matchDetailDocument.getHomeStats());
         ReflectionTestUtils.setField(saved, "awayScore", matchDetailDocument.getAwayScore());
-        if (matchDetailDocument.getAwayShootOutScore() != null) {
-            ReflectionTestUtils.setField(saved, "awayShootOutScore", matchDetailDocument.getAwayShootOutScore());
-        }
-        ReflectionTestUtils.setField(saved, "awayStats", matchDetailDocument.getAwayStats());
-
-        AdditionalTime newAdditionalTime = matchDetailDocument.getAdditionalTime();
-        if (newAdditionalTime != null) {
-            AdditionalTime existingAdditionalTime = saved.getAdditionalTime();
-
-            if (existingAdditionalTime == null) {
-                // 기존 추가시간 객체가 아예 없었다면 객체 자체를 통째로 할당
-                ReflectionTestUtils.setField(saved, "additionalTime", newAdditionalTime);
-            } else {
-                // 기존 추가시간 객체가 있다면, 하위 필드(전반/후반) 별로 null이 아닐 때만 병합
-                if (newAdditionalTime.getFirstHalf() != null) {
-                    ReflectionTestUtils.setField(existingAdditionalTime, "firstHalf", newAdditionalTime.getFirstHalf());
-                }
-                if (newAdditionalTime.getSecondHalf() != null) {
-                    ReflectionTestUtils.setField(existingAdditionalTime, "secondHalf", newAdditionalTime.getSecondHalf());
-                }
-                if (newAdditionalTime.getExtraFirstHalf() != null) {
-                    ReflectionTestUtils.setField(existingAdditionalTime, "extraFirstHalf", newAdditionalTime.getExtraFirstHalf());
-                }
-                if (newAdditionalTime.getExtraSecondHalf() != null) {
-                    ReflectionTestUtils.setField(existingAdditionalTime, "extraSecondHalf", newAdditionalTime.getExtraSecondHalf());
-                }
-            }
-        }
-    }
-
-    @Override
-    public void upsertMatchClock(Long matchId, MatchDetailDocument.MatchClock matchClock) {
-        MatchDetailDocument saved = database.get(matchId);
-
-        if (saved == null) {
-            MatchDetailDocument matchDetailDocument = MatchDetailDocument.builder()
-                    .id(matchId)
-                    .matchClock(matchClock)
-                    .build();
-
-            database.put(matchId, matchDetailDocument);
-            return;
-        }
-
-        ReflectionTestUtils.setField(saved, "matchClock", matchClock);
+        ReflectionTestUtils.setField(saved, "sportDetail", matchDetailDocument.getSportDetail());
     }
 
 }
