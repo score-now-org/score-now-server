3183fcbd93c51619d0b7e46f755a8c57a7de6d97
zoolake <junhomoon224@gmail.com>
2026-08-06 16:26:57 +0900

    feat : spring data mongo @TypeAlias 를 위한 Config 파일 추가

---
+++ src/main/java/com/scorenow/scorenow_api/global/config/MongoMappingConfig.java
@@ -0,0 +1,28 @@
+package com.scorenow.scorenow_api.global.config;
+
+import com.scorenow.scorenow_api.domain.match.document.sportdetail.football.FootballDetail;
+import org.springframework.boot.autoconfigure.domain.EntityScanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoManagedTypes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class MongoMappingConfig {
+
+    // MatchDetailDocument 의 SportDetail 에 대해서 새로운 종목 추가 시 여기도 추가해야 함.
+    @Bean
+    public MongoManagedTypes mongoManagedTypes(ApplicationContext applicationContext) throws ClassNotFoundException {
+        Set<Class<?>> managedTypes = new HashSet<>(
+                new EntityScanner(applicationContext).scan(Document.class)
+        );
+
+        managedTypes.add(FootballDetail.class);
+
+        return MongoManagedTypes.fromIterable(managedTypes);
+    }
+}
