443595d8822f45c4093ea64cbcc38b3b0fd05553
zoolake <junhomoon224@gmail.com>
2026-09-02 02:23:21 +0900

    hotfix : 누락 파일 재반영

--- src/main/java/com/scorenow/scorenow_api/domain/league/controller/AdminLeagueSeasonStandingsController.java
+++ src/main/java/com/scorenow/scorenow_api/domain/league/controller/AdminLeagueSeasonStandingsController.java
@@ -58,7 +58,8 @@ public class AdminLeagueSeasonStandingsController implements AdminLeagueSeasonSt
             @PathVariable Long leagueSeasonId,
             @RequestPart("image") MultipartFile image) {
 
-        adminLeagueSeasonStandingsService.uploadLeagueSeasonStandingsImage(leagueSeasonId, image);
+        // TODO: 리그 순위 이미지 업로드는 일단 막아두고, 추후 이미지 서버 구축 및 관련 기능 개발할 때 처리하기.
+//        adminLeagueSeasonStandingsService.uploadLeagueSeasonStandingsImage(leagueSeasonId, image);
         return ApiResponse.success();
     }
 
